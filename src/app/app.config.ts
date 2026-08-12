e0a4eec5a855154442801945cf634b285a71cf81
Emilly Gomes De Souza <eg1007300825@gmail.com>
2026-08-12 11:36:03 -0300

    angular dia 12

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay} from 
 '@angular/platform-browser';
+import { provideHttpClient, withFetch, withInterceptors} from '@angular/common/http';
+import { httpInterceptor } from './core/interceptors/http.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideClientHydration(withEventReplay())
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch(), 
+    withInterceptors([httpInterceptor])),
   ]
 };
